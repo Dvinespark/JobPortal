41f2dfc112c4cfb2c5997a8faeee2274f63f120f
Shubhas <nimtsubash@gmail.com>
2021-11-13 19:56:55 -0500

    shubhas: web app created

--- webapp/views.py
+++ webapp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render, HttpResponse
 
 
 # Create your views here.
-def index(request):
-    return render(request, 'dashboard.html', context={})
+def home(request):
+    return HttpResponse("This is from webapp view.")

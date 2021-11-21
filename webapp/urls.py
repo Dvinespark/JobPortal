b76b88412c6a47f83ef736ee18e34142899fd591
Shubhas <nimtsubash@gmail.com>
2021-11-21 11:02:01 -0500

    login/logout form completed

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,4 +3,6 @@ from .views import *
 app_name = "webapp"
 urlpatterns = [
     path('', home, name="home"),
+    path('login', login_request, name="login"),
+    path('logout', logout_request, name="logout"),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,16 +1,17 @@
 import json
 
-from django.contrib.auth import authenticate, logout
+from django.contrib.auth import authenticate, logout, login
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 from django.core.serializers.json import DjangoJSONEncoder
+from django.urls import reverse
+
 from dashboard.db_helper import get_rows
-from forms import UserRegisterForm
+from .forms import UserRegisterForm
 from django.contrib import messages
 
 
-# Create your views here.
-def home(request):
+def home_data():
     sql = """
         SELECT id, 
             job_title,
@@ -26,12 +27,17 @@ def home(request):
             date(created_at) as created_at
         from employer;
     """
+    results = get_rows(sql)
+    return json.dumps(results, cls=DjangoJSONEncoder)
+
+
+# Create your views here.
+def home(request):
     # for user login and register
     register_form = UserRegisterForm()
     login_form = AuthenticationForm()
-    results = get_rows(sql)
     context = {
-        'data': json.dumps(results, cls=DjangoJSONEncoder),
+        'data': home_data(),
         'register_form': register_form,
         'login_form': login_form
     }
@@ -51,12 +57,17 @@ def register_request(request):
         return redirect("webapp:home")
 
 
-def login(request):
+def login_request(request):
+    print("function triggered")
     if request.method == "POST":
-        form = AuthenticationForm(request, data=request.POST)
+        print(request.POST)
+        form = AuthenticationForm(request, data=request.POST or None)
+        print(form)
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
+            print(username)
+            print(password)
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
@@ -65,11 +76,18 @@ def login(request):
             else:
                 messages.error(request, "Invalid username or password.")
         else:
-            messages.error(request, "Invalid username or password.")
+            register_form = UserRegisterForm()
+            login_form = form
+            context = {
+                'data': home_data(),
+                'register_form': register_form,
+                'login_form': login_form
+            }
+            return render(request, 'index.html', context=context)
     return redirect("webapp:home")
 
 
 def logout_request(request):
     logout(request)
     messages.info(request, "You have successfully logged out.")
-    return redirect("webapp:home")
+    return redirect('webapp:home')

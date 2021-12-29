2a5727b3929c1f147974dee727bca4c4794b3e68
Shubhas <nimtsubash@gmail.com>
2021-12-29 15:06:50 -0500

    db changed

--- portal/settings.py
+++ portal/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'testdb',
-        'USER': 'root',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
